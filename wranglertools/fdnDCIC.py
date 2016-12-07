10b4ca36b219b0c96cc271f49d0121717e0be239
Koray Kırlı <koray_kirli@hms.harvard.edu>
2016-12-07 17:46:51 -0500

    +

--- wranglertools/fdnDCIC.py
+++ wranglertools/fdnDCIC.py
@@ -39,7 +39,7 @@ class FDN_Connection(object):
         me_page = self.server + 'me'
         r = requests.get(me_page, auth=self.auth)
         self.check = False
-        if r.status_code == 200:
+        if r.status_code == 307:
             self.check = True
 
 
