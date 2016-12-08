51d014f8190fc31f7b0f21880d6df7bab5e54839
Koray Kırlı <koray_kirli@hms.harvard.edu>
2016-12-08 14:30:26 -0500

    more test fixes

--- wranglertools/import_data.py
+++ wranglertools/import_data.py
@@ -386,6 +386,14 @@ def combine_set(post_json, existing_data, sheet, accumulate_dict):
     return post_json, accumulate_dict
 
 
+def fix_attribution(post_json, connection):
+    if not post_json.get('lab'):
+        post_json['lab'] = connection.lab
+    if not post_json.get('award'):
+        post_json['award'] = connection.award
+    return post_json
+
+
 def excel_reader(datafile, sheet, update, connection, patchall,
                  dict_patch_loadxl, dict_replicates, dict_exp_sets, dict_file_sets):
     """takes an excel sheet and post or patched the data in."""
@@ -414,6 +422,7 @@ def excel_reader(datafile, sheet, update, connection, patchall,
         total += 1
         post_json = dict(zip(keys, values))
         post_json = build_patch_json(post_json, fields2types)
+        post_json = fix_attribution(post_json, connection)
         # add attchments here
         if post_json.get("attachment"):
             attach = attachment(post_json["attachment"])
