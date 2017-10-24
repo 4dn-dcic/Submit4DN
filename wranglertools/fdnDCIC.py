788cb11107288b23fcd6fadb1aa0bbd25ff96f5f
aschroed <andrew_schroeder@hms.harvard.edu>
2017-10-24 12:36:21 -0400

    Added ExperimentDamid to sheet order

--- wranglertools/fdnDCIC.py
+++ wranglertools/fdnDCIC.py
@@ -290,7 +290,7 @@ sheet_order = [
     "FileFastq", "FileFasta", "FileProcessed", "FileReference", "FileCalibration",
     "FileSet", "FileSetCalibration", "FileMicroscopy", "ImagingPath", "ExperimentMic",
     "ExperimentHiC", "ExperimentCaptureC", "ExperimentRepliseq", "ExperimentAtacseq", "ExperimentChiapet",
-    "ExperimentSeq", "ExperimentSet", "ExperimentSetReplicate", 'WorkflowRunSbg']
+    "ExperimentDamid", "ExperimentSeq", "ExperimentSet", "ExperimentSetReplicate", 'WorkflowRunSbg']
 
 # Most fields are covered by "exclude_from:submit4dn" tag for removal
 # do_not_use list can be populated if there are additional fields that nneds to be taken out
