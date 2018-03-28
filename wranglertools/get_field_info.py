74d96282012fe984ac92db569b16d121a0ee1b99
Sarah <sarah_reiff@hms.harvard.edu>
2018-03-28 16:21:46 -0400

    Removed print statement used for testing

--- wranglertools/get_field_info.py
+++ wranglertools/get_field_info.py
@@ -184,7 +184,6 @@ def get_uploadable_fields(connection, types, include_description=False,
                           include_comments=False, include_enums=False):
     fields = {}
     for name in types:
-        print(name)
         schema_name = name + '.json'
         uri = '/profiles/' + schema_name
         schema_grabber = fdnDCIC.FDN_Schema(connection, uri)
