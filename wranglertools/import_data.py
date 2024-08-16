cad104b8abe0ed53435d66239335c864d8e6a5d8
aschroed <andrew_schroeder@hms.harvard.edu>
2024-08-16 14:59:26 -0400

    bug fix for creds for extrafiles; version bump

--- wranglertools/import_data.py
+++ wranglertools/import_data.py
@@ -894,34 +894,21 @@ def update_item(verb, file_to_upload, post_json, filename_to_post, extrafiles, c
             pp.Path(filename_to_post).unlink()
     if extrafiles:
         extcreds = e['@graph'][0].get('extra_file_creds')
-        if extcreds:
-            print("existing ext creds")
-            import pdb; pdb.set_trace()
-            if (
-                datetime.datetime.strptime(extcreds.get('upload_credentials').get('Expiration'), "%Y-%m-%d %H:%M:%S%z")
-                .replace(tzinfo=None) < datetime.datetime.now()
-            ):
-                print("expired")
-                import pdb; pdb.set_trace()
-        else:
-            time.sleep(5)
+        if not extcreds or (
+            datetime.datetime.strptime(extcreds.get('upload_credentials').get('Expiration'), "%Y-%m-%d %H:%M:%S%z")
+            .replace(tzinfo=None) < datetime.datetime.now()
+        ):
             extcreds = get_upload_creds(e['@graph'][0]['accession'], connection, extfilecreds=True)
-            for fformat, filepath in extrafiles.items():
-                try:
-                    file_format = ff_utils.get_metadata(fformat, key=connection.key)
-                    ff_uuid = file_format.get('uuid')
-                except Exception:
-                    raise "Can't find file_format item for %s" % fformat
-                for ecred in extcreds:
-                    if ff_uuid == ecred.get('file_format'):
-                        upload_creds = ecred.get('upload_credentials')
-                        if (
-                            datetime.datetime.strptime(upload_creds.get('Expiration'), "%Y-%m-%d %H:%M:%S%z")
-                            .replace(tzinfo=None) < datetime.datetime.now()
-                        ):
-                            print("expired")
-                            import pdb; pdb.set_trace()
-                        upload_extra_file(upload_creds, filepath)
+        for fformat, filepath in extrafiles.items():
+            try:
+                file_format = ff_utils.get_metadata(fformat, key=connection.key)
+                ff_uuid = file_format.get('uuid')
+            except Exception:
+                raise "Can't find file_format item for %s" % fformat
+            for ecred in extcreds:
+                if ff_uuid == ecred.get('file_format'):
+                    upload_creds = ecred.get('upload_credentials')
+                    upload_extra_file(upload_creds, filepath)
     return e
 
 
@@ -1449,10 +1436,12 @@ def user_workflow_reader(workbook, sheet, connection):
 
 def get_upload_creds(file_id, connection, extfilecreds=False):  # pragma: no cover
     creds2return = 'upload_credentials'
-    url = f"{file_id}/upload/"
+    url = f"{file_id}/upload/?datastore=database"
     if extfilecreds:
         creds2return = 'extra_files_creds'
-        req = ff_utils.authorized_request(f"{connection.key.get('server')}/{url}", auth=ff_utils.get_authentication_with_server(connection.key)).json()
+        req = ff_utils.authorized_request(
+            f"{connection.key.get('server')}/{url}", auth=ff_utils.get_authentication_with_server(connection.key)
+        ).json()
     else:
         req = ff_utils.post_metadata({}, url, key=connection.key)
     return req['@graph'][0][creds2return]
@@ -1501,7 +1490,6 @@ def upload_file(creds, path):  # pragma: no cover
             options = {"shell": True}
         subprocess.check_call(command, env=env, **options)
     except subprocess.CalledProcessError as e:
-        import pdb; pdb.set_trace()
         raise RuntimeError("Upload failed with exit code %d" % e.returncode)
     else:
         end = time.time()
